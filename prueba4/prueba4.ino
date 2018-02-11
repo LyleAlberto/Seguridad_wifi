b7ddf0a057483f84d6666bf9f684b6da0382d401
LyleRomero <lylearomero@gmail.com>
2018-02-11 15:44:55 -0500

    prueba funciones conexion y key
    compilado pruebas de funciones para conexion wifi con servidor y key de peticion

--- prueba4/prueba4.ino
+++ prueba4/prueba4.ino
@@ -7,11 +7,10 @@ const char* password = "789332411817";
 
 const char* host = "34.212.125.15";
 String key;
+WiFiClient client;
 
 void comunicacion()
 {
-  StaticJsonBuffer<200> jsonBuffer;
-  WiFiClient client;
   Serial.printf("\n[Connecting to %s ... ", host);
   if (client.connect(host, 80))
   {
@@ -23,36 +22,30 @@ void comunicacion()
                             "\r\n";
     client.print(request);
     Serial.print(request);
-
+    
     Serial.println("[Response:]");
-    while (client.connected())
+    peticionKey();
+    client.stop();
+    Serial.println("\n[Disconnected]");
+  }
+  else
+  {
+    Serial.println("connection failed!]");
+    client.stop();
+  }
+}
+void peticionKey()
+{
+  StaticJsonBuffer<200> jsonBuffer;
+  while (client.connected())
     {
       if (client.available())
       {
         String response = client.readString();
         //Serial.println(line);    
-
         int index_first = response.lastIndexOf('{');
         int index_last = response.lastIndexOf('}');
         String stringJSON = response.substring(index_first, index_last + 1);
-
-        // Check HTTP status
-        /*char status[32] = {0};
-        client.readBytesUntil('\r', status, sizeof(status));
-        
-        if (strcmp(status, "HTTP/1.1 200 OK") != 0) {
-          //Serial.print(F("Unexpected response: "));
-          //Serial.println(status);
-          //return;
-        }
-         
-      
-        // Skip HTTP headers
-        char endOfHeaders[] = "\r\n\r\n";
-        if (!client.find(endOfHeaders)) {
-          Serial.println(F("Invalid response"));
-          //return;
-        }*/
 
         const size_t capacity = JSON_OBJECT_SIZE(3) + JSON_ARRAY_SIZE(2) + 60;
         DynamicJsonBuffer jsonBuffer(capacity);
@@ -74,18 +67,10 @@ void comunicacion()
       
         // Disconnect
         client.stop();
-        
       }
-    }
-    client.stop();
-    Serial.println("\n[Disconnected]");
-  }
-  else
-  {
-    Serial.println("connection failed!]");
-    client.stop();
-  }
+    } 
 }
+
 
 void setup()
 {
