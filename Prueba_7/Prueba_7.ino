9d8906ffa7c40b4b38234e9579455f6c7caf0906
Squirrel18 <nando966@hotmail.es>
2018-04-29 19:51:56 -0500

    Check one time connection

--- Prueba_7/Prueba_7.ino
+++ Prueba_7/Prueba_7.ino
@@ -13,8 +13,82 @@ const char* streamId   = "....................";
 const char* privateKey = "....................";
 int ledPin = BUILTIN_LED;
 WiFiServer server(80);
+boolean alreadyConnected = false; // whether or not the client was connected previously
 
-void wificonect(const char* ssid, const char* password){
+void setup() {
+  Serial.begin(115200);
+  
+  boolean result = WiFi.softAP("ESPsoftAP_01", "pass-to-soft-AP");
+  if (result == true) {
+    Serial.println("Ready");
+  } else {
+    Serial.println("Failed!");
+  }
+  Serial.println("Server started");
+}
+
+void loop() {
+
+  server.begin();
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+ 
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while (!client.available()){
+    delay(1);
+  }
+ 
+  // Read the first line of the request
+  String request = client.readStringUntil('\r');
+  Serial.println(request);
+  client.flush();
+ 
+  // Match the request
+ 
+  int value = LOW;
+  if (request.indexOf("id") && request.indexOf("pass") != -1) {
+    int pos = request.indexOf("id");
+    if (request.indexOf("=") != -1) {
+      int pos1 = request.indexOf("=");
+      if (request.indexOf("&") != -1) {
+        int pos2 = request.indexOf("&");     
+        String stringresponse = request.substring(pos1+1 , pos2);
+        char id[stringresponse.length()];
+        stringresponse.toCharArray(id, stringresponse.length()+1);
+        Serial.println(stringresponse);
+        if (request.indexOf("&&") != -1) {
+          int pos1 = request.indexOf("&&");
+          String stringresponse = request.substring(pos1+2 , request.length());
+          if (stringresponse.indexOf("=") != -1) {
+            int pos1 = stringresponse.indexOf("=");
+            String stringresponse1 = stringresponse.substring(pos1+1 , request.length());  
+            Serial.println(stringresponse1);
+          
+            char pass[stringresponse1.length()];
+            stringresponse1.toCharArray(pass, stringresponse1.length()-8);
+            server.close();
+            //WiFi.softAPdisconnect();
+            if (!alreadyConnected) {
+              wificonnect(id,pass);
+              alreadyConnected = true;
+            }
+                
+            digitalWrite(ledPin, HIGH);
+            value = HIGH;
+          } 
+        }  
+      }
+    }
+  }
+
+  Serial.printf("Stations connected = %d\n", WiFi.softAPgetStationNum());
+  delay(3000);
+}
+
+void wificonnect(const char* ssid, const char* password) {
   Serial.print(ssid);
   Serial.println(password);
 //  WiFi.begin(ssid, password);
@@ -30,86 +104,3 @@ void wificonect(const char* ssid, const char* password){
   Serial.println("IP address: ");
   //Serial.println(WiFi.localIP());
 }
-//}
-
-void setup() {
-  Serial.begin(115200);
-  
-  boolean result = WiFi.softAP("ESPsoftAP_01", "pass-to-soft-AP");
-  if(result == true)
-  {
-    Serial.println("Ready");
-  }
-  else
-  {
-    Serial.println("Failed!");
-  }
-  
-  Serial.println("Server started");
-}
-
-int value = 0;
-
-void loop() {
-
-  server.begin();
-  WiFiClient client = server.available();
-  if (!client) {
-    return;
-  }
- 
-  // Wait until the client sends some data
-  Serial.println("new client");
-  while(!client.available()){
-    delay(1);
-  }
- 
-  // Read the first line of the request
-  String request = client.readStringUntil('\r');
-  Serial.println(request);
-  client.flush();
- 
-  // Match the request
- 
-  int value = LOW;
-  if (request.indexOf("id") && request.indexOf("pass") != -1) {
-  int pos=request.indexOf("id");
-      if(request.indexOf("=")!= -1){
-      int pos1=request.indexOf("=");
-          if(request.indexOf("&")!= -1){
-          int pos2=request.indexOf("&");     
-          String stringresponse = request.substring(pos1+1 , pos2);
-          char id[stringresponse.length()];
-          stringresponse.toCharArray(id, stringresponse.length()+1);
-          Serial.println(stringresponse);
-              if(request.indexOf("&&")!= -1){
-              int pos1=request.indexOf("&&");
-              String stringresponse = request.substring(pos1+2 , request.length());
-                  if(stringresponse.indexOf("=")!= -1){
-                  int pos1=stringresponse.indexOf("=");
-                  String stringresponse1 = stringresponse.substring(pos1+1 , request.length());  
-                  Serial.println(stringresponse1);
-            
-                  char pass[stringresponse1.length()];
-                  stringresponse1.toCharArray(pass, stringresponse1.length()-8);
-                  //Serial.println(sid);Serial.println(assword);
-                  server.close();
-                  //WiFi.softAPdisconnect();
-                  //while(chongo==1){
-                  wificonect(id,pass);
-                     
-                  digitalWrite(ledPin, HIGH);
-                  value = HIGH;
-                 } 
-              }  
-           }
-        }
-    }
-    //conexionred(stringresponse,stringresponse1);
-    
-    Serial.printf("Stations connected = %d\n", WiFi.softAPgetStationNum());
-  delay(3000);
-  
-
-}
-
